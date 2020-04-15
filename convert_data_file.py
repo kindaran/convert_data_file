484afecf197658f85f79a42b9cd34f43d97d6e42
kindaran <kindaran@outlook.com>
2020-04-15 14:46:17 -0600

    Ongoing development

--- convert_data_file.py
+++ convert_data_file.py
@@ -1,7 +1,10 @@
 import logging
 import sys
+import os
+import glob
 
 import json
+import pandas as ps
 
 ##########
 ## Classes
@@ -9,16 +12,120 @@ import json
 class DataFile:
 
     def __init__(self,key,fileContent):
-        self.fileType = key
-        self.filePrefix = fileContent["file_prefix"]
 
+        try:
+            self.__loadConfig(key,fileContent)
+        except Exception as e:
+            msg = str(e)
+            logging.error("*****Error in Datafile.init(). Error: %s" % (msg))
     #END DEF
+
+    def __loadConfig(self,key,fileContent):
+        try:
+            self.fileType = key
+            self.filePrefix = fileContent["file_prefix"]
+            self.sourcePath = fileContent["source_dir"]
+            self.destPath = fileContent["target_dir"]
+            self.archivePath = fileContent["archive_dir"]
+            self.fileFormat = fileContent["file_format"]
+            self.delimiter = fileContent["delimeter"]
+        except Exception as e:
+            msg = str(e)
+            logging.error("*****Error in Datafile.__loadConfig(). Error: %s" % (msg))
+    #END DEF
+            
+    def __getFileList(self,p_path):
+        try:
+            logging.debug("***GETTING INPUT DIR FILE LIST")
+            filelist = [file for file in glob.glob(p_path + r"\*") if
+                        not os.path.isdir(file) and not "_OUTPUT" in file.upper()]
+            logging.debug("Found %s files in directory %s" % (len(filelist), p_path))
+            if len(filelist) > 0:
+                return filelist
+            else:
+                logging.warning("***WARNING: no files found in path %s matching search criteria" % (p_path))
+                return None
+            # end if
+        except Exception as e:
+            msg = str(e)
+            logging.error("*****Error in Datafile.__getFileList. p_path: %s  Error: %s" % (p_path, msg))
+            return None
+
+    def searchSource(self):
+        try:
+            logging.info("*****LOOKING FOR FILES")
+            #get file list using FileTypes filters
+            fileList = self.__getFileList(self.sourcePath) 
+            if fileList == None:
+                logging.info("Unable to find files in source - exiting")
+                return False
+            #END IF
+        
+            self.fileList = [file for file in fileList if self.filePrefix.upper() + "_" in file.upper()]
+            logging.debug("Filelist: %s" % (self.fileList))
+            return True
+        except Exception as e:
+            msg = str(e)
+            logging.error("*****Error in Datafile.searchSource(). Error: %s" % (msg))
+            return False
+    #END IF
+
+    def loadData(self):
+
+        self.fileData = []
+
+        try:
+            logging.info("*****LOADING FILE DATA")
+
+            for file in self.fileList:
+                if self.fileFormat == "delimited":
+                    self.fileData.append(ps.read_csv(file,delimiter=self.delimiter,encoding='latin'))
+                elif self.fileFormat == "excel":
+                    self.fileData.append(ps.read_excel(file))
+                else:
+                    logging.error("Unknown file format %s - exiting" %(self.fileFormat))
+                    return False
+                #END IF
+            #END FOR
+            logging.debug("File data:\r\n %s" %(self.fileData))
+            return True
+        except Exception as e:
+            msg = str(e)
+            logging.error("*****Error in Datafile.searchSource(). Error: %s" % (msg))
+            return False
+    #END DEF
+
 #END CLASS
 
 ##########
 ## Functions
 ##########
+def getArgs():
+
+    args = []
+
+    try:
+        logging.debug("***COMMAND LINE ARGS HARD CODED")
+        if len(sys.argv) == 1:
+            configFilePath = "." + "\\" # sys.argv[1] + "\\"
+            args.append(configFilePath)
+            configFileName = "banks.json" #sys.argv[2]
+            args.append(configFileName)
+            logging.debug("Config file path would be %s" %(configFilePath + configFileName))
+            return args
+        else:
+            logging.error("Not enough arguments provided.")
+            print("Incorrect arguments provided\r\nPlease include path to config file and file name")
+            return None
+    except Exception as e:
+        msg = str(e)
+        logging.error("*****Error in getArgs. Args: %s  Error: %s" % (sys.argv,msg))
+        return None
+#END DEF
+    
 def loadConfigFile(p_path):
+
+    data = None
 
     try:
         with open(p_path,'r') as read_file:
@@ -39,42 +146,49 @@ def loadConfigFile(p_path):
 ##########
 def main():
 
-    fileTypes = []
+    dataFile = []
+    args = []
 
     try:
         ##get command line args
-        logging.debug("***COMMAND LINE ARGS HARD CODED")
-        if len(sys.argv) == 1:
-            configFilePath = "." + "\\" # sys.argv[1] + "\\"
-            configFileName = "banks.json" #sys.argv[2]
-            logging.debug("Config file path would be %s" %(configFilePath + configFileName))
-        else:
-            logging.error("Not enough arguments provided.")
-            print("Incorrect arguments provided\r\nPlease include path to config file and file name")
+        logging.info("*****GET COMMAND LINE ARGS")
+        args = getArgs()
+        if args == None:
+            logging.info("Unable to retrieve command line arguments - ending")
             return
         #END IF
+        configFilePath = args[0] + args[1]
 
         #process JSON config file
+        logging.info("*****LOAD CONFIG FILE")
         configFile = None
-        configFile = loadConfigFile(configFilePath + configFileName)
+        configFile = loadConfigFile(configFilePath)
         if configFile == None:
             logging.info("Unable to retrieve config file - ending")
             return
         #END IF
 
-        for fileType in configFile:
-            fileTypes.append(DataFile(fileType,configFile[fileType]))
+        logging.info("*****PROCESS CONFIG FILE")
+        #instantiate DataFile objects - one for each "file type" in the config file
+        dataFile = [DataFile(fileType,configFile[fileType]) for fileType in configFile]
+        logging.debug("Data file types: %s" %([file.fileType for file in dataFile]))
+
+        #main loop
+        logging.info("*****MAIN PROCESSING LOOP")
+        for file in dataFile:
+            logging.info("Processing file type %s" %(file.fileType))
+            if not file.searchSource():
+                logging.warning("Unable to find any source file for current file type")
+                continue
+            #END
+            if not file.loadData():
+                logging.warning("Unable to load data for current file type")
+                continue
+            #END IF
+            
+
         #END FOR
 
-        if logging.root.level == logging.DEBUG:
-            logging.debug("Config file types")
-            for fileType in fileTypes:
-                logging.debug("%s" %(fileType.fileType))
-                logging.debug("%s" %(fileType.filePrefix))
-        #END IF
-
-        filePrefixes = [fileType.filePrefix for fileType in fileTypes]
-        logging.debug("Prefixes: %s" %(filePrefixes))
     except Exception as e:
         msg = str(e)
         logging.error("*****Error in Main. Error: %s" % (msg))      
@@ -92,13 +206,13 @@ def main():
 loggingLevel = logging.DEBUG
 
 logging.basicConfig(level=loggingLevel, format="%(levelname)s: %(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
-logging.info('*****PROGRAM START') 
+logging.info('*****PROGRAM START*****') 
 
 if __name__ == "__main__":
     main()
 #END IF
 
-logging.info('*****PROGRAM END')  
+logging.info('*****PROGRAM END*****')  
 
 
 
